6c942bd01969bfa5e299da786d606615ff6c7a79
Your Name <angeltcastillo11@gmail.com>
2022-10-31 20:06:52 -0500

    .

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -1,32 +1,35 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import {Header} from './components/Header';
-import {Watchlist}from './components/Watchlist';
-import {Watched} from './components/Watched';
-import {Add}  from "./components/Add";
-import './App.css';
+import { Header } from "./components/Header";
+import { Watchlist } from "./components/Watchlist";
+import { Watched } from "./components/Watched";
+import { Add } from "./components/Add";
+import "./App.css";
 import "./lib/font-awesome/css/all.min.css";
+
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-   <Router>
-    <Header/>
+    <GlobalProvider>
+      <Router>
+        <Header />
 
-    <Route>
-      <Route exact path="/">
-        <Watchlist />
-      </Route>
-
-      <Route path="/Watched">
-        <Watched />
-      </Route>
-
-      <Route path="/add">
-        <Add />
-      </Route>
-    </Route>
-   </Router>
+        <Switch>
+          <Route exact path="/">
+            <Watchlist />
+          </Route>
+          <Route path="/add">
+            <Add />
+          </Route>
+          <Route path="/watched">
+            <Watched />
+          </Route>
+        </Switch>
+      </Router>
+    </GlobalProvider>
   );
 }
 
 export default App;
+
